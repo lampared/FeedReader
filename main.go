fd5a1c790cb089c44ced38d8aaaa61fe2a1a788d
lampared <98248264+lampared@users.noreply.github.com>
2022-11-01 15:27:42 +0100

    v2.4.1 Added Log to File

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"FeedReader/services"
 	"fmt"
+	"log"
+	"log/syslog"
 	"net/http"
 )
 
 func main() {
 	//v2.4.1
+	logWriter, err := syslog.New(syslog.LOG_SYSLOG, "FeedReader")
+	if err != nil {
+		log.Fatalln("Unable to set logfile:", err.Error())
+	}
+	// set the log output
+	log.SetOutput(logWriter)
+
 	fmt.Println("-- FEED READER SERVER START -- ")
 	//define the handlers for the the services request
 
