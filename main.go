8d322b2d84fabb088d0a5e533a95271e47cf5432
lampared <98248264+lampared@users.noreply.github.com>
2022-11-01 17:40:23 +0100

    2/7.0

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	version := "2.6.1"
+	version := "2.7.0"
 	logWriter, err := os.Create("feedreader.log")
 	if err != nil {
 		log.Fatalln("Unable to set logfile:", err.Error())
